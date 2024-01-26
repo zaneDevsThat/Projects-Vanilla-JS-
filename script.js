140c703ea18dfc89beb317c2674a9f42ea2e51f6
zaneDevsThat <zane.webdev@gmail.com>
2024-01-26 21:26:55 +0800

    fight, defeat monster added; locations objects updated

--- script.js
+++ script.js
@@ -36,6 +36,23 @@ const weapons = [
         power: 100,
     },
 ];
+const monsters = [
+    {
+        name: "slime",
+        level: 2,
+        health: 15,
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 60,
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health: 300,
+    },
+]
 
 const locations = 
 [
@@ -81,6 +98,26 @@ const locations =
         ],
         text: "You enter the cave. You see some monsters.",
     },
+    {
+        name: "fight",
+        "button text": [
+            "Attack",
+            "Dodge",
+            "Run"
+        ],
+        "button functions": [
+            attack,
+            dodge,
+            goTown
+        ],
+        text: "You are fighting a monster.",
+    },
+    {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
+    },
 ];
 
 
@@ -92,6 +129,7 @@ button3.onclick = fightDragon;
 
 function update(location)
 {
+    monsterStats.style.display = "none";
     button1.innerText = location["button text"][0];
     button1.onclick = location["button functions"][0];
     button2.innerText = location["button text"][1];
@@ -193,19 +231,69 @@ function goCave()
     update(locations[2]);
 }
 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function goFight()
+{
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsters[fighting].health;
+}
+
+function attack()
+{
+    text.innerText = "The " + monsters[fighting].name + " attacks."
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+        lose();
+    } 
+    else if (monsterHealth <= 0) 
+    {
+        defeatMonster();
+    }
+}
+
+function dodge()
+{
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
+
 function fightSlime()
 {
-
+    fighting = 0;
+    goFight();
 }
 
 function fightBeast()
 {
-
+    fighting = 1;
+    goFight();
 }
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 function fightDragon()
 {
-    console.log("Fighting dragon.");
+    fighting = 2;
+    goFight();
 }
 
+function defeatMonster()
+{
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+}
+
+function lose()
+{
+   update(locations[5]);
+}
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
