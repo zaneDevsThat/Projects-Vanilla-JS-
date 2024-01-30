ea46efcfc62b81df2a18820584b88f95a2028440
zaneDevsThat <zane.webdev@gmail.com>
2024-01-30 13:40:07 +0800

    Finished Calorie Counter project, started the phase 1 of Music Player App

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -56,6 +56,54 @@ function addEntry() {
     //Same as onclick on the 1st project(RPG), we can call the .addEventListener() method of the addEntryButton. It takes two arguments. The first is the event to listen to – you should pass the string click. The second is the callback function, or the function that runs when the event is triggered. Pass the addEntry function as the second argument. Note that you should not call addEntry, but pass the variable (or function reference) directly.
 }
 
+function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+  
+    const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+    const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+    const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+    const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+    const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+  
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+}
+
+function calculateCalories(e) {
+    e.preventDefault();
+    isError = false;
+
+    const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+    const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+    const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+    const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+    const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+    const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+    const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+    const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]); //Cause NodeLists are array-like. And this function's parameter is only accepting NodeLists and not elements that why we placed it in an array
+
+    if (isError) {
+        return;
+    }
+
+    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+    const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+    const surplusOrDeficit = remainingCalories >= 0 ? 'Surplus' : 'Deficit';
+    output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>
+    `;
+
+    output.classList.remove('hide');
+}
+
 function getCaloriesFromInputs(list) {
     let calories = 0;
   
@@ -71,6 +119,21 @@ function getCaloriesFromInputs(list) {
       calories += Number(currVal); //Number constructor which converts strings into numbers
     }
     return calories;
-  }
+}
+
+function clearForm() 
+{
+    const inputContainers = Array.from(document.querySelectorAll('.input-container')); //Array is a native object in javascript which is array-like and returns an array. It has a method .from()
+    for (let i = 0; i < inputContainers.length; i++)
+    {
+        inputContainers[i].innerHTML = ""; //to clear all the contents of the current input container iterated
+    }
+    budgetNumberInput.value = '';
+    output.innerText = '';
+    output.classList.add('hide');
+}
+  
 
 addEntryButton.addEventListener('click', addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener('click', clearForm);
